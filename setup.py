c7f2cabc84619f3718be9ff6f2e5e852c9322881
IAmCrowfunder@gmail.com <IAmCrowfunder@gmail.com>
2019-10-25 15:19:09 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fresh_script',
+    version='',
+    packages=[''],
+    url='https://github.com/amcquade/fresh_script',
+    license='MIT',
+    author='amcquade',
+    author_email='',
+    description='Find Spotify tracks posted to the HipHopHeads subreddit and add them to a Spotify playlist.'
+)
